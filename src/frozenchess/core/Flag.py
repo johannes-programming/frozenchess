ac697df1f9ba89ff3a90110e3b003421ae162149
Johannes <johannes-programming@mailfence.com>
2025-05-13 21:00:46 +0200

    Version 0.0.0+local.2

--- src/frozenchess/core/Flag.py
+++ src/frozenchess/core/Flag.py
@@ -25,72 +25,95 @@ class Flag(enum.IntEnum):
 
     #
     def __invert__(self) -> Self:
+        "This magic method implements ~self."
         return self.__op1(self, operator.invert)
 
     # Binary operators
     def __and__(self, other: SupportsIndex, /) -> Self:
-        self.__op2(self, other, operator.and_)
+        "This magic method implements self & other."
+        return self.__op2(self, other, operator.and_)
 
     def __or__(self, other: SupportsIndex, /) -> Self:
-        self.__op2(self, other, operator.or_)
+        "This magic method implements self | other."
+        return self.__op2(self, other, operator.or_)
 
     def __xor__(self, other: SupportsIndex, /) -> Self:
-        self.__op2(self, other, operator.xor)
+        "This magic method implements self ^ other."
+        return self.__op2(self, other, operator.xor)
 
     def __add__(self, other: SupportsIndex, /) -> Self:
-        self.__op2(self, other, operator.add)
+        "This magic method implements self + other."
+        return self.__op2(self, other, operator.add)
 
     def __sub__(self, other: SupportsIndex, /) -> Self:
-        self.__op2(self, other, operator.sub)
+        "This magic method implements self - other."
+        return self.__op2(self, other, operator.sub)
 
     def __mul__(self, other: SupportsIndex, /) -> Self:
-        self.__op2(self, other, operator.mul)
+        "This magic method implements self * other."
+        return self.__op2(self, other, operator.mul)
 
     def __floordiv__(self, other: SupportsIndex, /) -> Self:
-        self.__op2(self, other, operator.floordiv)
+        "This magic method implements self // other."
+        return self.__op2(self, other, operator.floordiv)
 
     def __mod__(self, other: SupportsIndex, /) -> Self:
-        self.__op2(self, other, operator.mod)
+        "This magic method implements self % other."
+        return self.__op2(self, other, operator.mod)
 
     def __pow__(self, other: SupportsIndex, /) -> Self:
-        self.__op2(self, other, operator.pow)
+        "This magic method implements self ** other."
+        return self.__op2(self, other, operator.pow)
 
     def __lshift__(self, other: SupportsIndex, /) -> Self:
-        self.__op2(self, other, operator.lshift)
+        "This magic method implements self << other."
+        return self.__op2(self, other, operator.lshift)
 
     def __rshift__(self, other: SupportsIndex, /) -> Self:
-        self.__op2(self, other, operator.rshift)
+        "This magic method implements self >> other."
+        return self.__op2(self, other, operator.rshift)
 
     # Reverse operators
     def __rand__(self, other: SupportsIndex, /) -> Self:
-        self.__op2(other, self, operator.and_)
+        "This magic method implements other & self."
+        return self.__op2(other, self, operator.and_)
 
     def __ror__(self, other: SupportsIndex, /) -> Self:
-        self.__op2(other, self, operator.or_)
+        "This magic method implements other | self."
+        return self.__op2(other, self, operator.or_)
 
     def __rxor__(self, other: SupportsIndex, /) -> Self:
-        self.__op2(other, self, operator.xor)
+        "This magic method implements other ^ self."
+        return self.__op2(other, self, operator.xor)
 
     def __radd__(self, other: SupportsIndex, /) -> Self:
-        self.__op2(other, self, operator.add)
+        "This magic method implements other + self."
+        return self.__op2(other, self, operator.add)
 
     def __rsub__(self, other: SupportsIndex, /) -> Self:
-        self.__op2(other, self, operator.sub)
+        "This magic method implements other - self."
+        return self.__op2(other, self, operator.sub)
 
     def __rmul__(self, other: SupportsIndex, /) -> Self:
-        self.__op2(other, self, operator.mul)
+        "This magic method implements other * self."
+        return self.__op2(other, self, operator.mul)
 
     def __rfloordiv__(self, other: SupportsIndex, /) -> Self:
-        self.__op2(other, self, operator.floordiv)
+        "This magic method implements other // self."
+        return self.__op2(other, self, operator.floordiv)
 
     def __rmod__(self, other: SupportsIndex, /) -> Self:
-        self.__op2(other, self, operator.mod)
+        "This magic method implements other % self."
+        return self.__op2(other, self, operator.mod)
 
     def __rpow__(self, other: SupportsIndex, /) -> Self:
-        self.__op2(other, self, operator.pow)
+        "This magic method implements other ** self."
+        return self.__op2(other, self, operator.pow)
 
     def __rlshift__(self, other: SupportsIndex, /) -> Self:
-        self.__op2(other, self, operator.lshift)
+        "This magic method implements other << self."
+        return self.__op2(other, self, operator.lshift)
 
     def __rrshift__(self, other: SupportsIndex, /) -> Self:
-        self.__op2(other, self, operator.rshift)
+        "This magic method implements other >> self."
+        return self.__op2(other, self, operator.rshift)
